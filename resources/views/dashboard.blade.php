6a5f24bf9a117eee9470bf24a33149fec3a0c6a4
ShodanMaster <akshaykrishnan922@gmail.com>
2025-07-30 03:09:57 +0530

    Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+
+@section('content')
+<h1 class="mb-4">Dashboard</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('login', [LoginController::class, 'index'])->name('login');
 Route::post('login-in', [LoginController::class, 'loginIn'])->name('loginin');
+
+Route::middleware('auth')->group(function(){
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
